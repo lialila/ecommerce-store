97888b2fbd56077cd7727bbdb3c4bae9a089d4cd
Uliana Cheklina <uche0790@gmail.com>
2023-02-11 21:03:10 +0100

    Add the input instead of buttons

--- app/shop/[itemName]/Item.js
+++ app/shop/[itemName]/Item.js
@@ -1,19 +1,15 @@
 'use client';
-import Image from 'next/image';
-// import { useState } from 'react';
+import { useState } from 'react';
 import { getParsedCookie, setStringifiedCookie } from '../../../utils/cookies';
 
-// const [itemsQuantity, setItemsQuantity] = useState('1');
-// import { itemsWithAmount } from '../shop/page';
-
-// import styles from './page.module.scss';
-
 export default function Item(props) {
+  const [itemsQuantity, setItemsQuantity] = useState(1);
+
   return (
     <>
       <img
         data-test-id="product-image"
-        src={`/images/${props.item.type}-${props.item.id}.jpg`}
+        src={`/images/${props.item.type}-${props.item.id}.png`}
         alt={props.item.type}
         width="160"
         height="160"
@@ -26,65 +22,61 @@ export default function Item(props) {
         <p data-test-id="product-image">{props.item.price}</p>€
       </div>
       <div>
-        <label for="amount">Add product to your cart</label>
-        <input
-          value="quantity"
-          data-test-id="product-quantity"
-          // onChange={(e) => setItemsQuantity(e.target.value)}
-        />
-        <button
-          onClick={() => {
-            //get the cookie
-            const itemsInCookies = getParsedCookie('itemsCookie');
+        <form name="inputnumber">
+          <label htmlFor="amount">Add product to your cart</label>
+          <input
+            type="number"
+            min="0"
+            value={itemsQuantity}
+            data-test-id="product-quantity"
+            pattern="^\+?(0|[1-9]\d*)$"
+            required
+            onChange={(e) => {
+              setItemsQuantity(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            data-test-id="product-add-to-cart"
+            onClick={() => {
+              //get the cookie
+              const itemsInCookies = getParsedCookie('itemsCookie');
+              console.log('getParsedCookie', itemsInCookies);
 
-            //if there is no cookie, do nothing
-            if (!itemsInCookies) {
-              return;
-            }
-            const foundItem = itemsInCookies.find((itemInCookie) => {
-              return itemInCookie.id === props.item.id;
-            });
-            // if the item is inside the cookie
-            if (foundItem) {
-              foundItem.amount--;
-              if (foundItem.amount < 0) {
-                foundItem.amount = 0;
+              // if there is no cookie, do nothing
+              if (itemsQuantity * 1 !== NaN && itemsQuantity * 1 >= 0) {
+                if (!itemsInCookies) {
+                  setStringifiedCookie('itemsCookie', [
+                    {
+                      id: props.item.id,
+                      amount: Math.floor(itemsQuantity * 1),
+                    },
+                  ]);
+                  return;
+                }
+                const foundItem = itemsInCookies.find((itemInCookie) => {
+                  return itemInCookie.id === props.item.id;
+                });
+                // if the item is inside the cookie
+                if (foundItem) {
+                  console.log('itemsQuantity', itemsQuantity);
+                  foundItem.amount += Math.floor(itemsQuantity * 1);
+                } else {
+                  itemsInCookies.push({
+                    id: props.item.id,
+                    amount: Math.floor(itemsQuantity * 1),
+                  });
+                }
+              } else {
+                return;
               }
+              console.log('items quantity', +itemsQuantity);
               setStringifiedCookie('itemsCookie', itemsInCookies);
-            }
-          }}
-        >
-          -
-        </button>
-        <button
-          onClick={() => {
-            //get the cookie
-            const itemsInCookies = getParsedCookie('itemsCookie');
-
-            //if there is no cookie, do nothing
-            if (!itemsInCookies) {
-              setStringifiedCookie('itemsCookie', [
-                { id: props.item.id, amount: 1 },
-              ]);
-              return;
-            }
-            const foundItem = itemsInCookies.find((itemInCookie) => {
-              return itemInCookie.id === props.item.id;
-            });
-            // if the item is inside the cookie
-            if (foundItem) {
-              foundItem.amount++;
-            } else {
-              itemsInCookies.push({ id: props.item.id, amount: 1 });
-            }
-
-            setStringifiedCookie('itemsCookie', itemsInCookies);
-          }}
-        >
-          +
-        </button>
-        <button data-test-id="product-add-to-cart">Add to card</button>
-        <h2>In your cart: {props.item.amount}</h2>
+            }}
+          >
+            Add to cart
+          </button>
+        </form>
       </div>
     </>
   );
--- app/shop/page.js
+++ app/shop/page.js
@@ -1,4 +1,3 @@
-// import { Alternates, Monoton, Montserrat, Questrial } from '@next/font/google';
 import { cookies } from 'next/headers';
 import Image from 'next/image';
 import Link from 'next/Link';
@@ -6,59 +5,62 @@ import { shop } from '../../database/shop';
 import styles from './page.module.scss';
 
 export default function ShopPage(props) {
+  const itemsCookie = cookies().get('itemsCookie');
 
-const itemsCookie = cookies().get('itemsCookie');
+  let itemsCookieParsed = [];
 
-let itemsCookieParsed = [];
-
-if (itemsCookie) {
-  itemsCookieParsed = JSON.parse(itemsCookie.value);
+  if (itemsCookie) {
+    itemsCookieParsed = JSON.parse(itemsCookie.value);
   }
 
   console.log('shop', shop);
 
   //adding property 'amount' to every item
-  const itemsWithAmount = shop.map((item)=> {
+  const itemsWithAmount = shop.map((item) => {
     const itemWithAmount = { ...item, amount: 0 };
 
     //read the cookie to find the item
     const itemInCookie = itemsCookieParsed.find(
-      itemObject => item.type === itemObject.id,
+      (itemObject) => item.type === itemObject.id,
     );
 
     //if find the item, update the amount
-    if(itemInCookie) {
+    if (itemInCookie) {
       itemWithAmount.amount = itemInCookie.amount;
     }
     return itemWithAmount;
-  })
-
+  });
 
   return (
     <>
-      <Link href='/shop' data-test-id="products-link"
-><h1 className={styles.h1}>SHOP</h1></Link>
-       <main className={styles.main}>
+      <Link href="/shop" data-test-id="products-link">
+        <h1 className={styles.h1}>SHOP</h1>
+      </Link>
+      <main className={styles.main}>
         <ul>
-        {shop.map((item) => {
-          return (
-            <li key={item.id} className={styles.card}>
-            <Link href={`/shop/${item.id.toString()}`} data-test-id={`${item.name}-${item.id}`}>
-            <Image className={styles.img}
-              src={`/images/${item.type}-${item.id}.jpg`}
-              alt={item.type}
-              width="140"
-              height="140"
-            />
-            <h3>{item.name}</h3>
-            <h2>{item.price}</h2>
-            <h2>In your cart: {item.amount}</h2>
-            </Link>
-          </li>
-          );
-        })}
+          {shop.map((item) => {
+            return (
+              <li key={item.id} className={styles.card}>
+                <Link
+                  href={`/shop/${item.id.toString()}`}
+                  data-test-id={`${item.name}-${item.id}`}
+                >
+                  <Image
+                    className={styles.img}
+                    src={`/images/${item.type}-${item.id}.png`}
+                    alt={item.type}
+                    width="140"
+                    height="140"
+                  />
+                  <h3>{item.name}</h3>
+                  <h2>{item.price}</h2>
+                  <h2>In your cart: {item.amount}</h2>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </main>
-    </>,
+    </>
   );
 }
